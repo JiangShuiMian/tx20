35c14d5d89560dd4c55f9572348adced6963ab3b
wangqiang <wangqiang1@banggood.com>
2020-06-04 11:17:25 +0800

    完成数据预处理。。。

--- dc/build_edge_dict_user_graph.py
+++ dc/build_edge_dict_user_graph.py
@@ -13,6 +13,8 @@ from cfgs.config import o_train_data, o_test_data, graphsage_data_path_user_grap
 
 trian_click_log_data = os.path.join(o_train_data, "click_log.csv")
 test_click_log_data = os.path.join(o_test_data, "click_log.csv")
+trian_ad_data_file = os.path.join(o_train_data, "ad.csv")
+test_ad_data_file = os.path.join(o_test_data, "ad.csv")
 
 cols = ['user_id', 'creative_id']
 
@@ -21,14 +23,36 @@ test_pairs = pd.read_csv(test_click_log_data, encoding='utf-8', dtype=int)[cols]
 print("训练数据大小: %d" % (train_pairs.shape[0]))
 print("测试数据大小: %d" % (test_pairs.shape[0]))
 
+train_ad_data = pd.read_csv(trian_ad_data_file, encoding='utf-8', dtype=object)
+test_ad_data = pd.read_csv(test_ad_data_file, encoding='utf-8', dtype=object)
+train_ad_data['creative_id'] = train_ad_data['creative_id'].astype(int)
+test_ad_data['creative_id'] = test_ad_data['creative_id'].astype(int)
+
+train_pairs = pd.merge(train_pairs, train_ad_data, how='left', on='creative_id')
+test_pairs = pd.merge(test_pairs, test_ad_data, how='left', on='creative_id')
+
+train_pairs = train_pairs[['user_id', 'advertiser_id']]
+test_pairs = test_pairs[['user_id', 'advertiser_id']]
+
+print(train_pairs.columns)
+
 creative_id_user_list = {}
 
 print("读取训练数据中边。。。")
 
 train_pairs_userids = list(train_pairs['user_id'])
-train_pairs_creativeids = list(train_pairs['creative_id'])
+train_pairs_creativeids = list(train_pairs['advertiser_id'])
 test_pairs_userids = list(test_pairs['user_id'])
-test_pairs_creativeids = list(test_pairs['creative_id'])
+test_pairs_creativeids = list(test_pairs['advertiser_id'])
+
+# creative_id_user_list = {}
+#
+# print("读取训练数据中边。。。")
+#
+# train_pairs_userids = list(train_pairs['user_id'])
+# train_pairs_creativeids = list(train_pairs['creative_id'])
+# test_pairs_userids = list(test_pairs['user_id'])
+# test_pairs_creativeids = list(test_pairs['creative_id'])
 
 del train_pairs
 del test_pairs
--- dc/data_ee.py
+++ dc/data_ee.py
@@ -58,8 +58,6 @@ def read_data():
     for col in click_data.columns:
         print_value_counts(click_data, col)
 
-
-
     test_click_data = pd.read_csv(os.path.join(o_test_data, "click_log.csv"), encoding='utf-8')
 
     train_ad_creative_ids = set(list(click_data['creative_id']))
